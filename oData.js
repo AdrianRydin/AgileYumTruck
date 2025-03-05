69f11943d5fbad00ab0283cc69ee4bd2e98ec57c
AdrianRydin <adrian.rydin@gmail.com>
2025-03-05 14:43:40 +0100

    Inspiration code

---
+++ oData.js
@@ -0,0 +1,63 @@
+// let User = {
+//     userName: [],
+// }
+
+// let order = {
+//     Namn: "",
+//     Pris: "",
+//     Antal: "",
+//     TotalPris: ""
+    
+//     }
+
+// let referenceNumber = randomNum
+
+// referenceNumber = {
+
+// }
+
+// let orders = [
+//     {
+//         referenceNumber: randomNum,
+//         order: "1",
+//     },
+//     {
+//         referenceNumber: randomNum,
+//         order: "2",
+
+//     },
+//     {
+//         referenceNumber: randomNum,
+//         order: "3",
+
+//     }
+
+// ]
+
+
+// for (let i = 0; i < oData.users.length; i++) {
+//     if(username.input === oData.users[i].username && password.input === oData.users[i].password){
+//         "login"
+
+//     }
+// }
+
+// let userName = document.getElementById("input").value;
+// let password = document.getElementById("input").value;
+// let orders = [
+//     {
+
+//     }
+// ]
+// let user = {
+//     userName: userName,
+//     orders: orders
+// }
+
+// if(userName === localStorage.getItem("user").userName) {
+//     "spara"
+// }
+
+
+
+      
