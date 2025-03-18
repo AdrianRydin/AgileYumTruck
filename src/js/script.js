66c1fe17cbb64c59509af67d245aca9ba844069f
emblut <embla.holgersson@hotmail.com>
2025-03-18 21:36:36 +0100

    Todays business hours display

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,34 @@
-document.addEventListener('DOMContentLoaded', () =>{
+import { displayBusinessHours } from './todays-business-hours.js';
+import { initSlideshow } from './slideshow.js';
 
-
-    // caroussel för index.html
-
-    // spara bilderna i en array
-    // set timer och byt bild efter timer är slut
-
-
-    // matmeny antal mat
-
-    // Gör att span.textContent ökar eller minskar när man trycker på + eller -
-    // spara antalet som man väljer i #count på mat meny
-    // return antal
-
-    // matmeny mat
-
-    // spara matval i en array
-    // hämta antal från förra funktionen och lägg till det i array
-    // spara array i localStorage
-
-    // kundvagn
-
-    // ankalla funktionen clearContainer() 
-    // Hämta matval array från localStorage
-    
-    // kundvagn antal mat
-    
-    // vid tryck på + eller - hämta värdet av det matvalet och gör -1 eller +1
-    // spara nya värdet i localStorage
-
-    // visa totalpris
-
-    // (matval2[i].price * matval2[i].amount)
-
-
-    // bokningsbekräftelse SKAPA INLOGGNING/REGISTRERING FÖRST
-
-    // Skapa refensnummer till den individuella beställnigen
-    // Referensnummret blir +1 för varje beställning
-    // OM man är inloggad, spara beställningen i localStorage för användaren i orders
-
-    // kvitto
-
-    // Hämta beställningen från localStorage
-    // Visa pris, namn på produkten/produkter, antal för varje produkt, totalpris och referensnummret
-
-    
-
-})
+document.addEventListener('DOMContentLoaded', () => {
+  // Visa dagens öppetider
+  displayBusinessHours();
+  // caroussel för index.html
+  initSlideshow();
+  // spara bilderna i en array
+  // set timer och byt bild efter timer är slut
+  // matmeny antal mat
+  // Gör att span.textContent ökar eller minskar när man trycker på + eller -
+  // spara antalet som man väljer i #count på mat meny
+  // return antal
+  // matmeny mat
+  // spara matval i en array
+  // hämta antal från förra funktionen och lägg till det i array
+  // spara array i localStorage
+  // kundvagn
+  // ankalla funktionen clearContainer()
+  // Hämta matval array från localStorage
+  // kundvagn antal mat
+  // vid tryck på + eller - hämta värdet av det matvalet och gör -1 eller +1
+  // spara nya värdet i localStorage
+  // visa totalpris
+  // (matval2[i].price * matval2[i].amount)
+  // bokningsbekräftelse SKAPA INLOGGNING/REGISTRERING FÖRST
+  // Skapa refensnummer till den individuella beställnigen
+  // Referensnummret blir +1 för varje beställning
+  // OM man är inloggad, spara beställningen i localStorage för användaren i orders
+  // kvitto
+  // Hämta beställningen från localStorage
+  // Visa pris, namn på produkten/produkter, antal för varje produkt, totalpris och referensnummret
+});
